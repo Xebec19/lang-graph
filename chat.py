9e4d7a9fefce71e9db2dd31882697ad13b01b87d
xebec19 <rohandeveloper106@gmail.com>
2026-07-07 21:19:33 +0530

    $msg

--- chat.py
+++ chat.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 from typing_extensions import TypedDict
-from typing import Optional
+from typing import Optional, Literal
 from langgraph.graph import StateGraph, START, END
 from langchain.chat_models import init_chat_model
 
@@ -16,18 +16,47 @@ class State(TypedDict):
 
 
 def chatbot(state: State):
+
+    print("Chatbot Node")
+
     response = llm.invoke(state.get("user_query"))
 
     state["llm_output"] = response.content
 
-    return {"messages": [response]}
+    return state
 
 
-def evaluate_response(state: State):
+def chatbot_gemini(state: State):
+    response = llm.invoke(state.get("user_query"))
+
+    state["llm_output"] = response.content
+
+    return state
+
+
+def evaluate_response(state: State) -> Literal["chatbot_gemini", "endnode"]:
     if True:
-        return ""
+        return "endnode"
 
-    return ""
+    return "chatbot_gemini"
+
+
+def endnode(state: State):
+    return state
 
 
 graph_builder = StateGraph(State)
+
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_node("chatbot_gemini", chatbot_gemini)
+graph_builder.add_node("endnode", endnode)
+
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_conditional_edges("chatbot", evaluate_response)
+graph_builder.add_conditional_edges("chatbot_gemini", endnode)
+graph_builder.add_edge("endnode", END)
+
+graph = graph_builder.compile()
+
+updated_state = graph.invoke(State({"user_query": "Hey, What is 2+2"}))
+print(updated_state)
