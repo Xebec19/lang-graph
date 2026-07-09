23dd85be20df132dc1cfccdb2213777b34c87222
xebec19 <rohandeveloper106@gmail.com>
2026-07-09 22:02:46 +0530

    $msg

--- chat-checkpoint.py
+++ chat-checkpoint.py
@@ -1,39 +1,42 @@
 from dotenv import load_dotenv
-from typing_extensions import TypedDict
-from typing import Optional, Literal
-from langgraph.graph import StateGraph, START, END
+from langgraph.graph import StateGraph, START, END, MessagesState
 from langchain.chat_models import init_chat_model
+from langgraph.checkpoint.mongodb import MongoDBSaver
 
 load_dotenv()
 
 llm = init_chat_model("google_genai:gemini-2.5-flash-lite")
 
 
-class State(TypedDict):
-    user_query: str
-    llm_output: Optional[str]
-    is_good: Optional[str]
+def chatbot(state: MessagesState):
+
+    response = llm.invoke(state["messages"])
+
+    return {"messages": [response]}
 
 
-def chatbot(state: State):
-
-    print("Chatbot Node")
-
-    response = llm.invoke(state.get("user_query"))
-
-    state["llm_output"] = response.content
-
-    return state
-
-
-graph_builder = StateGraph(State)
+graph_builder = StateGraph(MessagesState)
 
 graph_builder.add_node("chatbot", chatbot)
 
 graph_builder.add_edge(START, "chatbot")
 graph_builder.add_edge("chatbot", END)
 
-graph = graph_builder.compile()
 
-updated_state = graph.invoke(State({"user_query": "Hey, What is 2+2"}))
-print(updated_state)
+def compile_graph_with_checkpointer(checkpointer):
+
+    return graph_builder.compile(checkpointer=checkpointer)
+
+
+DB_URI = "mongodb://admin:admin@localhost:27017/lg?authSource=admin"
+with MongoDBSaver.from_conn_string(DB_URI) as checkpointer:
+    graph_with_checkpointer = compile_graph_with_checkpointer(checkpointer)
+
+    config = {"configurable": {"thread_id": "123"}}
+
+    updated_state = graph_with_checkpointer.invoke(
+        MessagesState({"messages": [{"role": "user", "content": "What is my name ?"}]}),
+        config,
+    )
+
+    print(updated_state["messages"][-1].content)
